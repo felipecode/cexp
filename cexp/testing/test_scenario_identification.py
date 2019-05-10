0c1381e834d6691ce9280c9db22b9d5757e7fd79
felipecode <fcodevilla@cvc.uab.es>
2019-05-10 18:01:42 +0200

    increasing n and divigin

--- cexp/testing/test_scenario_identification.py
+++ cexp/testing/test_scenario_identification.py
@@ -6,14 +6,14 @@ from cexp.testing.server_test import start_test_server, check_test_server
 
 import carla
 
-def test_distance_intersection_speed(world, N=20):
+def test_distance_intersection_speed(world, N=100):
     # I will spawn N vehicles
 
     blueprint_library = world.get_blueprint_library()
     spawn_points = world.get_map().get_spawn_points()
     blueprint = random.choice(blueprint_library.filter('vehicle*'))
     capture = time.time()
-    count = 0
+    count = 1
     for point in spawn_points[:N]:
         vehicle = world.try_spawn_actor(blueprint, point)
         if vehicle is None:
@@ -21,7 +21,7 @@ def test_distance_intersection_speed(world, N=20):
         print("Spawn pont %d distance %f " %(count, distance_to_intersection(vehicle, world.get_map(), resolution=0.1)))
         count += 1
 
-    print ("Took  %f  seconds " % (time.time() - capture))
+    print ("Took  %f  seconds " % ((time.time() - capture)/count))
 
 
 if __name__ == '__main__':
