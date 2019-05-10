0200d180111a7371c6ea4947f043b96921f5454f
felipecode <fcodevilla@cvc.uab.es>
2019-05-10 17:58:58 +0200

    removing prints to test timne

--- cexp/env/scenario_identification.py
+++ cexp/env/scenario_identification.py
@@ -9,12 +9,9 @@ def distance_to_intersection(vehicle, wmap, resolution=0.1):
     total_distance = 0
     # TODO computational cost may be an issue keep track of the current waypoint.
     reference_waypoint = wmap.get_waypoint(vehicle.get_transform().location)
-    print ("reference")
-    print (reference_waypoint)
+
     while not reference_waypoint.is_intersection:
         reference_waypoint = reference_waypoint.next(resolution)[0]
-        print ("next reference")
-        print (reference_waypoint)
         total_distance += resolution
 
     return total_distance
