16b31a812fbe8ffd8d50d41bbe4e0fdd4915b43d
Felipe Codevilla <felipe.alcm@gmail.com>
2019-08-20 17:55:28 -0400

    another del

--- cexp/testing/test_carla_simple.py
+++ cexp/testing/test_carla_simple.py
@@ -51,7 +51,20 @@ class CarlaSyncMode(object):
         self.delta_seconds = 1.0 / kwargs.get('fps', 20)
         self._queues = []
         self._settings = None
+        self._settings = self.world.get_settings()
+        self.frame = self.world.apply_settings(carla.WorldSettings(
+            no_rendering_mode=True,
+            synchronous_mode=True))
+        def make_queue(register_event):
+            q = queue.Queue()
+            register_event(q.put)
+            self._queues.append(q)
 
+        make_queue(self.world.on_tick)
+        for sensor in self.sensors:
+            make_queue(sensor.listen)
+
+    
     def __enter__(self):
         self._settings = self.world.get_settings()
         self.frame = self.world.apply_settings(carla.WorldSettings(
