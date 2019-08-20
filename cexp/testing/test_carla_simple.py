b53d4e3521e6d12a5f7a6e7613568e37bceb8361
Felipe Codevilla <felipe.alcm@gmail.com>
2019-08-20 16:07:11 -0400

    another simple example for memory leak

--- cexp/testing/test_carla_simple.py
+++ cexp/testing/test_carla_simple.py
@@ -56,8 +56,7 @@ class CarlaSyncMode(object):
         self._settings = self.world.get_settings()
         self.frame = self.world.apply_settings(carla.WorldSettings(
             no_rendering_mode=False,
-            synchronous_mode=True,
-            fixed_delta_seconds=self.delta_seconds))
+            synchronous_mode=True))
 
         def make_queue(register_event):
             q = queue.Queue()
