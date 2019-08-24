37e3bdc4d6319d4279e7ff9b21ee2d9613b7b0fe
YiXiao <yixiao@cudahpc04.cvc.uab.es>
2019-08-24 18:57:58 +0200

    fix the memory leak

--- cexp/env/sensors/sensor_interface.py
+++ cexp/env/sensors/sensor_interface.py
@@ -178,7 +178,7 @@ class CallBack(object):
     def _parse_image_cb(self, image, tag, writer):
 
         array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
-        array = copy.deepcopy(array)  # TODO IS THIS NEEDED  ?
+
         array = np.reshape(array, (image.height, image.width, 4))
         array = array[:, :, :3]
         array = array[:, :, ::-1]
@@ -187,7 +187,7 @@ class CallBack(object):
     def _parse_lidar_cb(self, lidar_data, tag, writer):
 
         points = np.frombuffer(lidar_data.raw_data, dtype=np.dtype('f4'))
-        points = copy.deepcopy(points)
+        #points = copy.deepcopy(points)
         points = np.reshape(points, (int(points.shape[0] / 3), 3))
         self._data_provider.update_sensor(lidar_data, tag, points, lidar_data.frame_number, writer)
 
