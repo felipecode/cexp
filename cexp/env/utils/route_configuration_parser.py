aec0b6f6567bb09d4501d432a1f9e2446c4f61f9
Felipe Codevilla <felipe.alcm@gmail.com>
2019-08-24 15:05:49 -0400

    it is called trigger position

--- cexp/env/utils/route_configuration_parser.py
+++ cexp/env/utils/route_configuration_parser.py
@@ -343,7 +343,7 @@ def get_actors_instances(list_of_antagonist_actors):
     return list_of_actors
 
 def get_filtered_match_position(event, route):
-    waypoint  = convert_waypoint_float(event['transform'])
+    waypoint  = convert_waypoint_float(event['trigger_position'])
     match_position = match_world_location_to_route(waypoint, route)
 
     if match_position is not None:
