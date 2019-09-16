a096538b760fb7bbef2be21ed505b300239410bc
Felipe Codevilla <felipe.alcm@gmail.com>
2019-09-16 15:58:23 -0400

    setup fixing

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 
 setup(name='cexp', version='0.1',
 
-      packages=['cexp', 'cexp.driving_batch'],
+      packages=['cexp', 'cexp.env'],
 
       install_requires=['numpy'])
